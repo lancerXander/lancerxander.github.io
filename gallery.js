e6aa76e3ec4d6646003c53c7066f15f3fa1385c1
XanderLancer <55094083+lancerXander@users.noreply.github.com>
2020-02-24 05:54:23 -0500

    Update 2.0
    Cleanup and Image Gallery Flicker fix

--- gallery.js
+++ gallery.js
@@ -1,16 +1,6 @@
-document.addEventListener('click', function (event) {
-  console.log(event.target.parentElement.parentElement);
-  if (event.target.parentElement.parentElement.matches('.gallery-image__preview')) {
-    event.target.parentElement.parentElement.classList.remove('gallery-image__preview');
-    event.target.parentElement.parentElement.scrollIntoView();
-  } else if (event.target.matches('.gallery-image__media')) {
-    // Remove any existing preview classes
-    var previewing = document.getElementsByClassName('gallery-image__preview');
-    for (el of previewing) {
-      el.classList.remove('gallery-image__preview');
-    }
-		// Preview the clicked figure
-    event.target.parentElement.parentElement.classList.add('gallery-image__preview');
-    event.target.parentElement.parentElement.scrollIntoView();
-	}
-}, false);
+$(function() {
+		$('.pop').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('#imagemodal').modal('show');
+		});
+});
